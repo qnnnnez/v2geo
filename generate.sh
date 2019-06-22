bb382a701bc87c2686a9d3803dc36755fa5638e5
Wu Shuang <qnnnnez@live.com>
2019-06-22 15:43:56 +0800

    change output dat file name

--- generate.sh
+++ generate.sh
@@ -10,6 +10,6 @@ OUT_DIR="$DIR/output"
 mkdir -p $OUT_DIR
 cd $DIR
 bash pre.sh
-wget -O - $DNSMASQ_CHINA_LIST_URL | egrep -v '^#' | sed -e 's|^server=/\(.*\)/114.114.114.114$|domain:\1|' | python3 v2geo.py geosite $OUT_DIR/geosite.dat -i - CN
-wget -O - $CHINA_IP_LIST_URL | python3 v2geo.py geoip $OUT_DIR/geoip.dat -i - CN
+wget -O - $DNSMASQ_CHINA_LIST_URL | egrep -v '^#' | sed -e 's|^server=/\(.*\)/114.114.114.114$|domain:\1|' | python3 v2geo.py geosite $OUT_DIR/dnsmasqchinalist.dat -i - CN
+wget -O - $CHINA_IP_LIST_URL | python3 v2geo.py geoip $OUT_DIR/chinaiplist.dat -i - CN
 wget -O - $HOSTS_URL | egrep "^0.0.0.0 " | awk '{print "domain:"$2}' | python3 v2geo.py geosite $OUT_DIR/adsite.dat -i - AD
